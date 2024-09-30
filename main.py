92a216eb240a242de94af66d7eeefc5bbb00bea8
pegas164932 <pegas164932@gmail.com>
2024-09-30 23:36:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#практическое занятие БАЗОВЫЕ СТРУКТУРЫ ДАННЫХ
+print((9**0.5)*5)
+
+print(9.99>9.98 and 1000<1000.1)
+
+print(2+2*2)
+print((2+2)*2)
+print(6==8)
+
+str_a='123.456'
+b=float(str_a)
+print(b)
+print(int(b*10)%10)
